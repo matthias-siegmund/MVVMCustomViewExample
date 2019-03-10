0a5fe1a090f75d7cde6b4859aba6ce285f1d68b5
Matthias Siegmund <matthias.c.siegmund@gmail.com>
2019-03-10 21:35:14 +0100

    remove recyclerview example

--- app/src/main/java/com/example/matthias/mvvmcustomviewexample/MainActivity.kt
+++ app/src/main/java/com/example/matthias/mvvmcustomviewexample/MainActivity.kt
@@ -2,14 +2,10 @@ package com.example.matthias.mvvmcustomviewexample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = SampleAdapter().apply { items = listOf("A", "B") }
     }
 }
